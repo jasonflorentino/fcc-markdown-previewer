e68aa044c1111147417bdd6c9e54175b5a6618cd
Jason Florentino <jasonmflorentino@gmail.com>
2021-02-21 19:03:47 -0500

    Polish styling, implement sticky editor

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ ___
 
 **Write** or paste [markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) text on the left to see it \`previewed\` to the right.
 
-## This is a level two heading
+___
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+## Downsview Park
 
-> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+[Downsview Park](https://en.wikipedia.org/wiki/Downsview_Park) is a large urban park located in the Downsview neighborhood of Toronto, Ontario, Canada. The park's name is officially bilingual due to it being federally owned and managed, and was first home to de Havilland Canada, an aircraft manufacturer, and later was a Canadian Forces base.
+
+> This is an example of a quote. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 
 ### A code block, a list, and a puppy
 
@@ -30,6 +32,16 @@ print(greet("Bob"))
 * mollit anim id est laborum.
 
 ![A cute puppy](https://place-puppy.com/200x200 "A cute puppy")
+
+# The Editor is Sticky!
+
+---
+
+**As in it stays with you as your scroll. How convienient!**
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 `
 
 function Preview(props) {
@@ -66,7 +78,8 @@ class Previewer extends React.Component {
     event.preventDefault();
   }
 
-  copyToClipboard = () => {
+  copyToClipboard = (event) => {
+    console.log(event.target);
     const elem = this.textArea
     elem.select()
     document.execCommand("copy")
@@ -79,19 +92,21 @@ class Previewer extends React.Component {
   render() {
     return (
       <div id="Previewer">
-        <div id="inputForm" className="Previewer__section">
-          <div className="Previewer__header">
-            <p>Write or paste markdown here</p>
-            {
-              this.state.copySuccess ?
-              <div className="Previewer__success">
-                Copied to Clipboard!
-              </div> : null
-            }
-            <button className="Previewer__button" onClick={() => this.copyToClipboard()}>Copy</button>
+        <div id="stickyContainer">
+          <div id="inputForm" className="Previewer__section">
+            <div className="Previewer__header">
+              <p class="Previewer__title">⭐️  Welcome to my markdown previewer</p>
+              {
+                this.state.copySuccess ?
+                <div className="Previewer__success">
+                  Copied to Clipboard!
+                </div> : null
+              }
+              <button className="Previewer__button" onClick={(e) => this.copyToClipboard(e)}>Copy</button>
+            </div>
+            <textarea id="editor" value={this.state.value} onChange={this.handleChange} ref={(textarea) => this.textArea = textarea}/>
+            {/* <input type="submit" value="Submit" /> */}
           </div>
-          <textarea id="editor" value={this.state.value} onChange={this.handleChange} ref={(textarea) => this.textArea = textarea}/>
-          {/* <input type="submit" value="Submit" /> */}
         </div>
         <Preview markdown={this.state.value} />
       </div>
